c185655be5c9d5b1b029e7c4ee37ea7b4f7b8e26
Shaun Fourie <shaunfr777@gmail.com>
2025-01-27 16:12:16 +0200

    Update Day1String.py

--- Day1String.py
+++ Day1String.py
@@ -3,3 +3,10 @@ print("Welcome to the Band Name Generator.")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print("Your band name could be " + city + " " + pet)
+
+# Using variables to calc the length
+username = input("What is your name")
+length = len(username)
+print(length)
+# All in one line, this will 1st complete 'input', 2ndly 'len', 3rdly 'print'
+print(len(input("What is your name?")))
